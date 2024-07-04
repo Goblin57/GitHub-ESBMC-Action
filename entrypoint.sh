ca7a433cf586bebd4e60493f5dae0985633a54f9
n59321jh <joshua.hines@student.manchester.ac.uk>
2024-07-04 16:37:44 +0100

    LSVerifier install test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
 
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-
-if [ -f $1 ]; then
-    echo "File exists"
-    esbmc $1 $2
-else
-    echo "File does not exist."
-fi
-
-if [ $4 = "y" ]; then
-    lsverifier $5
-else
-    echo "LSVerifier disabled."
-fi
 
