2ebda610feac695d3a88d3d171bf84b037213fcb
n59321jh <joshua.hines@student.manchester.ac.uk>
2024-07-15 11:18:52 +0100

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#IFS='\n'
+IFS='\n'
 options=$1
 
 directory_recursive() {
     cd $1
-    ls -d */
-    for directory in $(ls -d */)
+    ls -1 -d */
+    for directory in $(ls -1 -d */)
     do
         directory_recursive $directory
     done
