e138d5ea94913a654f367f361e6b75ec2002e06f
n59321jh <joshua.hines@student.manchester.ac.uk>
2024-07-15 11:11:30 +0100

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ options=$1
 directory_recursive() {
     cd $1
     ls -d */
-    for directory in $(ls -1 -d */)
+    for directory in $(ls -d */)
     do
         directory_recursive $directory
     done
